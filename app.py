a90e88d708a13c20586dfcf27b66fd36fcfd5169
sushanttiwari98 <sushantktiwari98@gmail.com>
2022-03-15 15:31:32 +0530

    python game

---
+++ app.py
@@ -0,0 +1,77 @@
+import pygame
+from pygame import display, event, image
+import game_config as gc
+from animal import Animal
+from time import sleep
+
+
+def find_index(x,y):
+    row = y//gc.IMAGE_SIZE
+    col = x//gc.IMAGE_SIZE
+    index = row*gc.NUM_TILES_SIDE + col
+    return index
+
+pygame.init()
+
+display.set_caption('My game')              #title of game window
+screen = display.set_mode((512,512))        #size of game window
+
+matched = image.load('other_assets/matched.png')
+#screen.blit(matched, (0,0))                 #Blit method displays a surface on another surface
+#display.flip()
+
+running = True
+
+tiles = [Animal(i) for i in range(0,gc.NUM_TILES_TOTAL)]
+
+#sleep(0.4)
+#screen.blit(matched,(0,0))
+#display.flip()
+#sleep(0.4)
+
+current_images = []
+
+#game loop
+while running:
+    current_events = event.get()            #(pygame event module)all keyboard and mouse event using get function
+    for e in current_events:
+        if e.type == pygame.QUIT:
+            running = False
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_ESCAPE:
+                running = False
+        if e.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            index = find_index(mouse_x,mouse_y)
+            if index not in current_images:
+                current_images.append(index)
+            #current_images.append(index)
+            if len(current_images)>2:
+                current_images = current_images[1:]
+
+    screen.fill((255,255,255))
+    total_skipped = 0
+    for _,tile in enumerate(tiles):
+        image_i = tile.image if tile.index in current_images else tile.box
+        if not tile.skip:
+            screen.blit(image_i,(tile.col*gc.IMAGE_SIZE + gc.MARGIN, tile.row*gc.IMAGE_SIZE + gc.MARGIN))
+    
+    if len(current_images) == 2:
+        idx1, idx2 = current_images
+        if tiles[idx1].name == tiles[idx2].name:
+            tiles[idx1].skip = True
+            tiles[idx2].skip = True
+            sleep(0.4)
+            screen.blit(matched,(64,64))
+            display.flip()
+            sleep(0.4)  
+            #display.flip()
+            current_images = []    
+        else:
+            total_skipped += 1
+            display.flip()
+
+    if total_skipped == len(tiles):
+        running = False
+
+print('Good bye!')
